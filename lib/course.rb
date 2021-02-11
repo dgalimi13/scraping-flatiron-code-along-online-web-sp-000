a6dafd6a8ebda767419cdd29bfa473e4342da00e
danny galimi <dannygalimi13@aim.com>
2021-02-11 19:25:28 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,7 +3,7 @@ class Course
   
   @@all = []
   
-  def initialize()
+  def initialize
     @@all << self
   end 
   
@@ -11,8 +11,8 @@ class Course
     @@all
   end 
   
-  def reset_all
-    
+  def self.reset_all
+    @@all.clear
   end
 
 end 
